b1887e500f89fa94343b2bd8da10162b21c9542d
unknown <sadhumanishkumar@gmail.com>
2020-06-06 00:05:32 +0530

    Chores : Added logo | color change | nav-items positions change

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,11 +7,16 @@
         <title>Health4All Today<?php  if($title){ echo " | ".$title;} ?></title>
     <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">  
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css" >
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/youseelogo.css" media='screen,print'>
   
     <script src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
     <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
 
     <style>
+       a{
+        color:"black";
+      }
+
       .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
@@ -33,36 +38,41 @@
   <body class="d-flex flex-column h-100">
     <header>
   <!-- Fixed navbar -->
-  <nav class="navbar navbar-expand-md navbar-dark  bg-dark justify-content-between">
-    <a class="navbar-brand" href="<?php echo base_url();?>">Health4All.today</a>
+
+  <nav class="navbar navbar-expand-md navbar-light  bg-light justify-content-between">
+  
+    <a class="navbar-brand" href="https://yousee.in/c4c" target="_blank"><span style="position:absolute;font-size:2.7em;left:5%;top:-18px" class="logo logo-yousee"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+    <a class="navbar-brand" href="<?php echo base_url();?>" > <span style="position:absolute;left:10%;top:10px">Health4All.Today</style></a>
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <ul class="navbar-nav navbar-left mr-auto">
       <?php 
         $logged_in=$this->session->userdata('logged_in');
       if($logged_in) { ?>
         <li class="nav-item active">
-          <a class="nav-link" href="<?php echo base_url();?>admin">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="<?php echo base_url();?>admin" >Home <span class="sr-only">(current)</span></a>
         </li>
       </ul>
       <ul class="navbar-nav navbar-right ">  
           <li class="nav-item">
-             <a class="nav-link" href="#"> <?php echo $logged_in['username']." | " ; ?></a>
+             <a class="nav-link" href="#" style="text-decoration:none; color:black;"> <?php echo $logged_in['username']." | " ; ?></a>
           </li>
           <li class="dropdown">
-            <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown"><i class="fa fa-gear"></i> Settings <b class="caret"></b></a>
+            <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" style="text-decoration:none; color:black;" ><i class="fa fa-gear"></i> Settings <b class="caret"></b></a>
             <ul class="dropdown-menu">
               <a class="dropdown-item" href="<?php echo base_url()."admin/change_password";?>"><i class="fa fa-edit"></i> Change Password</a>
               <a class="dropdown-item" href="<?php echo base_url();?>admin/logout"><i class="fa fa-sign-out"></i> Logout</a>
             </ul>
           </li>
         <?php } else {?>
-          <li class="nav-item   <?php if(preg_match("^".base_url()."admin/login^",current_url())){ echo " active";}?>">
-            <a class="nav-link" href="<?php echo base_url()."admin/login";?>" style="text-decoration:none; color:white"><i class="fa fa-sign-in" style="color:white;"></i> Login</a>
-          </li>
-      </ul>
+        </ul>
+          <ul class="navbar-nav navbar-right">
+            <li class="nav-item   <?php if(preg_match("^".base_url()."admin/login^",current_url())){ echo " active";}?>">
+              <a class="nav-link" href="<?php echo base_url()."admin/login";?>" style="text-decoration:none; color:black;"><i class="fa fa-sign-in" style="color:black;"></i> Login</a>
+            </li>
+          </ul>
         <?php }?>
       <form class="form-inline mt-2 mt-md-0">
         <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
