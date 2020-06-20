b621c604b84e20e63de4b959a11fc6d0e7ed4c7a
unknown <sadhumanishkumar@gmail.com>
2020-06-20 18:11:18 +0530

    Feat : multiple group select

--- application/views/admin/create_question.php
+++ application/views/admin/create_question.php
@@ -66,11 +66,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </select>
             </div>
         </div>
-        <div class="row">
-            <div class="form-group col-md-6">
-                <label for="groupId">Select Group <span class="star" style="color:red"> *</span></label>
-                <select class="form-control" name="group" id="groupId" required>
-                    <option value="" selected disabled>--Select--</option>
+        <div class="groups_wrapper">
+            <div class="row">
+                <div class="form-group col-md-4">
+                    <label for="groupId">Select Group <span class="star" style="color:red"> *</span></label>
+                    <select class="form-control" name="group[]" id="group_1" onChange="filter_sub_groups('group_1' , 'sub_group_1')" required>
+                    <option  selected disabled>--Select--</option>
                     <?php
                         foreach($groups as $r){
                             echo "<option value='".$r->group_id."'";
@@ -79,20 +80,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         }
                     ?>
                 </select>
-            </div>
-            <div class="form-group col-md-6">
-                <label for="subGroupId">Select Sub Group</label>
-                <select class="form-control" name="sub_group" id="subGroupId">
-                    <option value="" selected disabled>--Select--</option>
-                    <?php
-                        foreach($sub_groups as $r){
-                            echo "<option value='".$r->sub_group_id."'";
-                            if($this->input->post('sub_group') && $this->input->post('sub_group') == $r->sub_group_id) echo " selected ";
-                            echo ">".$r->sub_group."</option>";
-                        }
-                    ?>
-                </select>
-            </div>
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="subGroupId">Select Sub Group</label>
+                    <select class="form-control" name="sub_group[]" id="sub_group_1">
+                        <option value="" selected >--Select--</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-1">
+                <label for="">Add</label>  
+                    <button type="button" class="btn btn-primary" id="addGroupAndSubGroup"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                </div>
+            </div>    
         </div>
         <div class="form-group">
             <label for="explanation">Question Explanation</label>
@@ -100,7 +99,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
         <label for="answerFields">Answers Options<span class="star" style="color:red"> *</span></label> 
         <label for="answerFields"> (<span style="color:green">&#10004;</span> for the correct option )</label> 
-        <div class="wrapper">
+        <div class="answer_options_wrapper">
             <div class="row">
                 <div class="form-group col-md-6">
                     <input type="text" class="form-control" name="answer_option[]" required/>
@@ -131,20 +130,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <script>
     $(function() {
 
-        var wrapper    = $(".wrapper"); //Input fields wrapper
+        var answer_options_wrapper    = $(".answer_options_wrapper"); //Input fields answer_options_wrapper
         var add_button = $("#add_fields"); //Add button class or ID
         var x = 2; //Initial input field is set to 1
         //When user click on add input button
         $(add_button).click(function(e){
                 e.preventDefault();
                 
-                $(wrapper).append(`
+                $(answer_options_wrapper).append(`
                     <div class="row">
                         <div class="form-group col-md-6">
                             <input type="text" name="answer_option[]" class="form-control" /> 
                         </div>
                         <div class="form-group col-md-2">
-                        <input type="hidden" name="correct_option[]" value="0" />
+                        <input type="hidden" name="correct_option[]" value="" />
                         <input type="checkbox" id="option_"${x} name="correct_option[${x}]" value="1" style="width: 25px;height: 25px;">
                         </div>
                         <div class="form-group col-md-1">
@@ -155,13 +154,67 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 x++ //input field increment
             });
         
-        //when user click on remove button
-        $(wrapper).on("click",".remove_field", function(e){ 
+        //when user click on remove button in answer options
+        $(answer_options_wrapper).on("click",".remove_field", function(e){ 
+            e.preventDefault();
+            $(this).parent().parent('div').remove();
+            x--; 
+        });
+
+        var count=2;
+        var groups_wrapper = $(".groups_wrapper"); //Groups and subgroup wrapper 
+        var addGroupAndSubGroup = $("#addGroupAndSubGroup");
+        $(addGroupAndSubGroup).click(function (e) { 
+            $(groups_wrapper).append(`<div class="row">
+                    <div class="form-group col-md-4">
+                    <label for="group">Select Group <span class="star" style="color:red"> *</span></label>
+                    <select class="form-control" name="group[]" id="group_${count}" onChange="filter_sub_groups('group_${count}' , 'sub_group_${count}')" required>
+                    <option value="" selected disabled>--Select--</option>
+                    <?php
+                        foreach($groups as $r){
+                            echo "<option value='".$r->group_id."'";
+                            if($this->input->post('group_name') && $this->input->post('group_name') == $r->group_id) echo " selected ";
+                            echo ">".$r->group_name."</option>";
+                        }
+                    ?>
+                </select>
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="subGroup">Select Sub Group</label>
+                    <select class="form-control" name="sub_group[]" id="sub_group_${count}">
+                        <option value="0" selected >--Select--</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-1">
+                <label for="">Remove</label>  
+                    <button class="btn btn-danger removeGroupAndSubGroup" ><i class="fa fa-trash" aria-hidden="true"></i></button>
+                </div>
+                </div`);
+                count++;   
+        });
+        //when user click on remove button in answer options
+        $(groups_wrapper).on("click",".removeGroupAndSubGroup", function(e){ 
             e.preventDefault();
             $(this).parent().parent('div').remove();
             x--; 
         });
 
 });
-
+    // function to filter sub_groups based on a selected group 
+    function filter_sub_groups(group , id){
+        // fetching list of all subgroups
+        // alert("#############");
+        var sub_groups = <?php echo json_encode($sub_groups); ?>;
+        var selected_group = $(`#${group}`).val();
+        var filtered_sub_groups;
+        $(`#${id}`).empty().append(`<option  selected>Sub Group</option>`);
+             filtered_sub_groups = $.grep(sub_groups , function(v){
+                return v.group_id == selected_group;
+            }) ;
+            // iterating the selected sub groups
+        $.each(filtered_sub_groups, function (indexInArray, valueOfElement) { 
+            const {sub_group_id ,sub_group} = valueOfElement;
+            $(`#${id}`).append($('<option></option>').val(sub_group_id).html(sub_group));
+        });
+    }
 </script>
