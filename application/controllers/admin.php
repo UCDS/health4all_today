9c56b12e098c423b880710039a0e84432c99511e
Manish <sadhumanishkumar@gmail.com>
2021-12-21 13:26:31 +0530

    Feat : added restrictions to actions at controller level

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -149,38 +149,46 @@ class Admin extends CI_Controller {
 
 	public function create_question(){
 		if($this->session->userdata('logged_in')){
-			$this->load->helper('form');
-			$this->load->helper('directory');
-			$this->load->library('form_validation');
-			$this->data['title']="Create Question";
-			$this->data['display_max_width'] = $this->master_model->get_defaults('display_max_width');
-			$this->load->view('templates/header' , $this->data);
-			$this->data['languages'] = $this->master_model->get_languages();
-			$this->data['groups'] = $this->master_model->get_groups();
-			$this->data['sub_groups'] = $this->master_model->get_sub_groups();
-			$this->data['question_levels'] = $this->master_model->get_question_levels();
-			
-			$images_list = directory_map("./assets/images/quiz",TRUE,FALSE);
-				foreach($images_list as &$image_name){
-					$image_name = pathinfo($image_name)['filename'];
-				}
-			sort($images_list);
-			$this->data['images_list']= $images_list;
-			$this->form_validation->set_rules('question','question','required');
-			if ($this->form_validation->run() === FALSE) {
-				$this->load->view('admin/create_question',$this->data);
-			} else {
-				if($this->master_model->create_question()){
-					$this->data['status']=200;
-					$this->data['msg']="Question created successfully";
+			$add_question_access = 0;
+			foreach($this->data['functions'] as $f){
+				if($f->user_function=="question" && $f->add) 	$add_question_access=1;  	
+			}
+			if($add_question_access){
+				$this->load->helper('form');
+				$this->load->helper('directory');
+				$this->load->library('form_validation');
+				$this->data['title']="Create Question";
+				$this->data['display_max_width'] = $this->master_model->get_defaults('display_max_width');
+				$this->load->view('templates/header' , $this->data);
+				$this->data['languages'] = $this->master_model->get_languages();
+				$this->data['groups'] = $this->master_model->get_groups();
+				$this->data['sub_groups'] = $this->master_model->get_sub_groups();
+				$this->data['question_levels'] = $this->master_model->get_question_levels();
+				
+				$images_list = directory_map("./assets/images/quiz",TRUE,FALSE);
+					foreach($images_list as &$image_name){
+						$image_name = pathinfo($image_name)['filename'];
+					}
+				sort($images_list);
+				$this->data['images_list']= $images_list;
+				$this->form_validation->set_rules('question','question','required');
+				if ($this->form_validation->run() === FALSE) {
 					$this->load->view('admin/create_question',$this->data);
 				} else {
-					$this->data['status']=500;
-					$this->data['msg']="Error creating question. Please retry.";
-					$this->load->view('admin/create_question',$this->data);
+					if($this->master_model->create_question()){
+						$this->data['status']=200;
+						$this->data['msg']="Question created successfully";
+						$this->load->view('admin/create_question',$this->data);
+					} else {
+						$this->data['status']=500;
+						$this->data['msg']="Error creating question. Please retry.";
+						$this->load->view('admin/create_question',$this->data);
+					}
 				}
+				$this->load->view('templates/footer' , $this->data);
+			} else {
+				show_404();
 			}
-			$this->load->view('templates/footer' , $this->data);
 		} else{
 			show_404();
 		}
@@ -216,52 +224,68 @@ class Admin extends CI_Controller {
 	}
 
 	public function create_group(){
-		if($this->session->userdata('logged_in') &&  $this->session->userdata('logged_in')['admin']==1){
-			$this->load->helper('form');
-			$this->load->library('form_validation');
-			$this->data['title']="Create Group";
-			$this->load->library('form_validation');
-			$this->load->view('templates/header',$this->data);
-			$this->form_validation->set_rules('group','group','required');
-			if ($this->form_validation->run() === FALSE){
-				$this->load->view('admin/create_group' , $this->data);
-			} else {
-				if($this->master_model->insert_group()){
-					$this->data['msg']="Group created successfully";
-					$this->load->view('admin/create_group',$this->data);
-				} else {
-					$this->data['msg']="Error creating group. Please retry.";
-					$this->load->view('admin/create_group',$this->data);
-				}
+		if($this->session->userdata('logged_in')){
+			$add_group_access = 0;
+			foreach($this->data['functions'] as $f){
+				if($f->user_function=="group" && $f->add) 	$add_group_access=1;  	
 			}
-			$this->load->view('templates/footer' , $this->data);
+			if($add_group_access){
+				$this->load->helper('form');
+				$this->load->library('form_validation');
+				$this->data['title']="Create Group";
+				$this->load->library('form_validation');
+				$this->load->view('templates/header',$this->data);
+				$this->form_validation->set_rules('group','group','required');
+				if ($this->form_validation->run() === FALSE){
+					$this->load->view('admin/create_group' , $this->data);
+				} else {
+					if($this->master_model->insert_group()){
+						$this->data['msg']="Group created successfully";
+						$this->load->view('admin/create_group',$this->data);
+					} else {
+						$this->data['msg']="Error creating group. Please retry.";
+						$this->load->view('admin/create_group',$this->data);
+					}
+				}
+				$this->load->view('templates/footer' , $this->data);
+			} else {
+				show_404();	
+			}
 		} else {
 			show_404();
 		}
 	}
 
 	public function create_sub_group(){
-		if($this->session->userdata('logged_in') &&  $this->session->userdata('logged_in')['admin']==1){
-			$this->load->helper('form');
-			$this->load->library('form_validation');
-			$this->data['title']="Create Sub Group";
-			$this->load->library('form_validation');
-			$this->load->view('templates/header',$this->data);
-			$this->data['groups'] = $this->master_model->get_groups();
-			$this->form_validation->set_rules('group_id','group_id','required');
-			$this->form_validation->set_rules('sub_group','sub_group','required');
-			if ($this->form_validation->run() === FALSE){
-				$this->load->view('admin/create_sub_group' , $this->data);
-			} else {
-				if($this->master_model->insert_sub_group()){
-					$this->data['msg']="Sub group created successfully";
-					$this->load->view('admin/create_sub_group',$this->data);
-				} else {
-					$this->data['msg']="Error creating sub group. Please retry.";
-					$this->load->view('admin/create_sub_group',$this->data);
-				}
+		if($this->session->userdata('logged_in')){
+			$add_sub_group_access = 0;
+			foreach($this->data['functions'] as $f){
+				if($f->user_function=="sub_group" && $f->add) 	$add_sub_group_access=1;  	
 			}
-			$this->load->view('templates/footer' , $this->data);
+			if($add_sub_group_access){
+				$this->load->helper('form');
+				$this->load->library('form_validation');
+				$this->data['title']="Create Sub Group";
+				$this->load->library('form_validation');
+				$this->load->view('templates/header',$this->data);
+				$this->data['groups'] = $this->master_model->get_groups();
+				$this->form_validation->set_rules('group_id','group_id','required');
+				$this->form_validation->set_rules('sub_group','sub_group','required');
+				if ($this->form_validation->run() === FALSE){
+					$this->load->view('admin/create_sub_group' , $this->data);
+				} else {
+					if($this->master_model->insert_sub_group()){
+						$this->data['msg']="Sub group created successfully";
+						$this->load->view('admin/create_sub_group',$this->data);
+					} else {
+						$this->data['msg']="Error creating sub group. Please retry.";
+						$this->load->view('admin/create_sub_group',$this->data);
+					}
+				}
+				$this->load->view('templates/footer' , $this->data);
+			} else {
+				show_404();	
+			}
 		} else {
 			show_404();
 		}
@@ -318,8 +342,12 @@ class Admin extends CI_Controller {
 	}
 
 	public function upload_image(){
-		if($this->session->userdata('logged_in') &&  $this->session->userdata('logged_in')['admin']==1){
-			// var_dump($_FILES);
+		if($this->session->userdata('logged_in')){
+			$add_image_access = 0;
+			foreach($this->data['functions'] as $f){
+				if($f->user_function=="image" && $f->add) 	$add_image_access=1;  	
+			}
+			if($add_image_access){
 				$this->load->helper('form');
 				$this->load->library('form_validation');
 				$this->data['title']="Upload image";
@@ -340,5 +368,8 @@ class Admin extends CI_Controller {
 			} else {
 				show_404();
 			}
+		} else {
+			show_404();
+		}
 	}
 }
