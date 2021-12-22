c8bcd564f83e0a16f85ab5bac899833808a43b60
Manish <sadhumanishkumar@gmail.com>
2021-12-22 23:35:21 +0530

    Feat : query to create user_signin table

--- db-updates/h4atoday.22.12.21.sql
+++ db-updates/h4atoday.22.12.21.sql
@@ -16,3 +16,16 @@ ALTER TABLE `user`
 --- Making username as unique 
 ---
 ALTER TABLE `user` ADD UNIQUE(`username`);
+
+---
+--- Creating user_signin table
+---
+DROP TABLE IF EXISTS `user_signin`;
+CREATE TABLE IF NOT EXISTS `user_signin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(60) NOT NULL,
+  `signin_date_time` datetime NOT NULL,
+  `is_success` tinyint(1) NOT NULL,
+  `details` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for storing the user login activities';
