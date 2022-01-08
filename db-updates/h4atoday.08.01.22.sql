0b00b1329af5bebc4b4501b1eb0f32ff19aa1e3a
Manish <sadhumanishkumar@gmail.com>
2022-01-08 23:12:27 +0530

    Feat : query to update timestamp on row update

--- db-updates/h4atoday.08.01.22.sql
+++ db-updates/h4atoday.08.01.22.sql
@@ -2,3 +2,8 @@
 --- Adding new column display to the groups table
 ---
 ALTER TABLE `groups` ADD `display` TINYINT NOT NULL DEFAULT '1' AFTER `default_group`;
+
+---
+---
+---
+ALTER TABLE answer_option CHANGE updated_datetime updated_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
